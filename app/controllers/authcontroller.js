c95267bd9202b6390103ab04e14846a5bb395467
Sasha Slacum <slacus08@gmail.com>
2017-08-31 19:13:44 -0400

    added signup route and functionality but am running into an error, serialized passwords

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -1,5 +1,20 @@
 var exports = module.exports = {}
 
-module.exports = function signup(req, res) {
+exports.signup = function(req, res) {
     res.render('signup');
-};
+}
+
+exports.signin = function(req, res) {
+    res.render('signin');
+}
+
+exports.dashboard = function(req, res) {
+    res.render('dashboard');
+}
+
+exports.logout = function(req, res) {
+    req.session.destroy(function(err) {
+        res.redirect('/');
+    });
+
+}
