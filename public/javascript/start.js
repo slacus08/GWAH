068768e373044501386d643bfea6fdaeab530214
Sofia Wicker <sofiazw@gmail.com>
2017-09-05 16:52:50 -0400

    updated who can click on what cards when

--- public/javascript/start.js
+++ public/javascript/start.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	var user;
+	var user = "";
 
 	//needs to come from firebase, which comes from sql
 	var cardsFromSql = ["fun1", "fun2","fun3","fun4","fun5","fun6","fun7","fun8","fun9","fun10","fun11","fun12","fun13","fun14","fun15","fun16","fun17","fun18","fun19"];
@@ -13,93 +13,95 @@ $(document).ready(function() {
 
 	var hand = [];
 
-	var czar = true;
+	var czar = false;
 
 	//the function that is associated with this needs to come from firebase.
 
 	  // Initialize Firebase
-	  var config = {
+	var config = {
 	    apiKey: "AIzaSyDSSNV1gjwiaCVLTvKSZwgW5wNoSZ6HMOs",
 	    authDomain: "gwah-199f8.firebaseapp.com",
 	    databaseURL: "https://gwah-199f8.firebaseio.com",
 	    projectId: "gwah-199f8",
 	    storageBucket: "gwah-199f8.appspot.com",
 	    messagingSenderId: "817131336094"
-	  };
-	  firebase.initializeApp(config);
+	};
+	  
+	firebase.initializeApp(config);
 
 	var database = firebase.database();
 
-	  database.ref().on("child_added", function(childSnapshot) {
+	database.ref().on("child_added", function(childSnapshot) {
 	    var data = childSnapshot.val();
-	    var usersCount;
-	    // console.log(data);
+		console.log(data);
+	});
 
-	    // users = data.users;
-	    //
-	    // if (data && data.users) {
-	    //     usersCount = data.users.length;
-	    //     $('playerCount').text(usersCount);
-	    // }
-	  });
+	var idCount = 0;
 
-	  var idCount = 0;
+	database.ref('count').on("value", function(snapshot) {
+	  if (snapshot.exists()) {
+		idCount = snapshot.val().idCount;
+		$("#user-count").html("Number Of Users: " + idCount)
+	  } 
+	});
 
-	  /////on-clicks
+	$("#user-count").html("Number Of Users: " + idCount)
+
 
 	//registers the user in the database
 
   	$('#register-user').click(function() {
 
-  		//the first person who logs in becomes czar.
+  		if(!user) {
 
-	    //I pull the new count from firebase(we should consider changing this to a child added so it's always updating)
+	  		//the first person who logs in becomes czar.
 
-	    database.ref().on("value", function(childSnapshot) {
-			idCount = childSnapshot.val().count.idCount;
-		});
+		    //I pull the new count from firebase(we should consider changing this to a child added so it's always updating)
 
-		//I add 1 to the new count.
+			//I add 1 to the new count.
 
-		database.ref("count").update({
-	    	"idCount": idCount + 1
-	    });
+		    //I set the user name
+		  	user = $('#username-input').val();
 
-	    //I set the user name
-	  	user = $('#username-input').val();
+		  	if(idCount >= 1) {
 
-	  	//I set the initial table in firebase. 
-	    database.ref($('#username-input').val()).set({
-	     // "username": $('#username-input').val(),
-	        "hand": 0, 
-	        "points": 0, 
-	        "czar": true
-	    });
+		  		database.ref($('#username-input').val()).set({
+		     // "username": $('#username-input').val(),
+			        "hand": 0,
+			        "selected": 0, 
+			        "points": 0, 
+			        "czar": false
+			    });
 
-	    ////I check to see if there is a player.  If there is, then I change czar to false. 
-	    if(idCount > 1) {
-			database.ref().on("value", function(childSnapshot) {
-				database.ref().child(user).update({"czar": false});
+			} else {
+
+				database.ref($('#username-input').val()).set({
+			        "czarSelected": 0, 
+			        "czar": true
+			    });
+
+			    czar = true;
+			};
+
+			database.ref("count").update({
+		    	"idCount": idCount + 1
+		    });
+
+		    updateCardsToFirebase(cardsFromSql, whiteCardsFromSql)
+
+		    database.ref().once("value", function(childSnapshot) {
+				whiteCards = childSnapshot.val().whiteCards;
+				hand = [whiteCards[0], whiteCards[1], whiteCards[2], whiteCards[3], whiteCards[4], whiteCards[5]];
 			});
 
-			czar = false;	
+			push6CardsFromLocal(hand);
+
+		} else {
+		  	alert("you have already joined the game");
 		}
-
-	    updateCardsToFirebase(cardsFromSql, whiteCardsFromSql)
-
-	    database.ref().once("value", function(childSnapshot) {
-			whiteCards = childSnapshot.val().whiteCards;
-			hand = [whiteCards[0], whiteCards[1], whiteCards[2], whiteCards[3], whiteCards[4], whiteCards[5]];
-		});
-
-		push6CardsFromLocal(hand);
-
-		$("#user-count").html("Number Of Users: " + idCount);
-
-	  });
+  	});
 
     $('.band').on('click', 'p', function() {
-			//grabs the selected card and displays it on the screen; Needs more functionality. 
         cardSelected = $(this).html();
         updateCardsWithSelected(cardSelected);
 	});
@@ -110,6 +112,17 @@ $(document).ready(function() {
   		dealtHandAppearsOnScreen();
 	});
 
+
+  	$('#start').click(function() {
+  		if(idCount >= 4 && czar == true){
+  			alert("The game has started!")
+  			singleBlackCardPlayed();
+  		} else if (idCount >= 4  || idCount < 4 && czar == false) {
+  			alert("Only the czar may draw a card!");
+  		} else if (idCount < 4 && czar == true) {
+  			alert("Please wait for the right number of players!")
+  		}
+	});
 
 	// $(document).on("click", '#start', function(event) {
 
@@ -129,9 +142,11 @@ $(document).ready(function() {
 	};
 
 	function push6CardsFromLocal(hand) {
-		database.ref().once("value", function(childSnapshot) {
-			database.ref().child(user).update({"hand": hand});
-		});
+		if(czar == false) {
+			database.ref().once("value", function(childSnapshot) {
+				database.ref().child(user).update({"hand": hand});
+			});
+		};
 	}
 
 
