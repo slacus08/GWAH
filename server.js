aad79ba156aa556ccffeecbabba5deec3c7a7cc2
Sasha Slacum <slacus08@gmail.com>
2017-09-07 19:50:47 -0400

    took master's version of passport and server

--- server.js
+++ server.js
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-var express = require('express');
-var app = express();
-var passport = require('passport')
-var session = require('express-session')
-var bodyParser = require('body-parser')
-var env = require('dotenv').load();
-var exphbs = require('express-handlebars')//Routes
-
-//For BodyParser
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
-
-// For Passport
-app.use(session({
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized:true
-})); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-
-//For Handlebars
-app.set('views', './app/views')
-app.engine('hbs', exphbs({
-    extname: '.hbs'
-}));
-app.set('view engine', '.hbs');
-
-
-app.get('/', function(req, res) {
-  res.send('Welcome to Passport with Sequelize');
-});
-
-//Models
-var models = require("./app/models");
-
-//Routes
-var authRoute = require('./app/routes/auth.js')(app, passport);
-
-//load passport strategies
-require('./config/passport.js')(passport, models.user);
-
-//Sync Database
-models.sequelize.sync().then(function() {
-    console.log('Nice! Database looks fine')
-}).catch(function(err) {
-    console.log(err, "Something went wrong with the Database Update!")
-});
-
-
-app.listen(3306, function(err) {
-    if (!err)
-        console.log("Site is live");
-    else console.log(err)
-});
-=======
 var express    = require('express')
     var app        = express()
     var passport   = require('passport')
@@ -139,5 +80,3 @@ var express    = require('express')
 		console.log("Site is live at port 3000"); else console.log(err)
 
 	});
-
->>>>>>> master
