59726ea9314a0d9ee3f6942fe3ee3b11a13a82d8
Sofia Wicker <sofiazw@gmail.com>
2017-09-07 20:31:55 -0400

    try a jaws thing'

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ var path      = require("path");
 var Sequelize = require("sequelize");
 var env       = process.env.NODE_ENV || "development";
 var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+var sequelize = new Sequelize(config.use_env_variable);
 var db        = {};
 
 
