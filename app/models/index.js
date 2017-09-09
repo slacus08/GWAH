82aa16ccb57629470b0b99f343d64b6467ed671d
Sofia Wicker <sofiazw@gmail.com>
2017-09-09 15:34:25 -0400

    one line for sequelize

--- app/models/index.js
+++ app/models/index.js
@@ -9,10 +9,7 @@ var sequelize = null;
 var db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize({
-    url: process.env.JAWSDB_URL,
-    dialect: 'mysql'
-  });
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
